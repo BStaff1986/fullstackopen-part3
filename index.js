573a0463e07af928e8da8e0434a68bb1d9b847f0
Bryan Stafford <BStaff@gmail.com>
2024-04-09 15:30:15 -0400

    First two exercises of part3

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+
+const persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    const currentDate = new Date().toLocaleString();
+    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
+    const numberOfPersons = persons.length;
+    
+    response.send(
+        `
+        <p>Phonebook has info for ${numberOfPersons} people</p>
+        <p>${currentDate} (${timeZone})</p>
+        `
+    )
+
+})
+
+const PORT = 3001;
+app.listen(PORT)
