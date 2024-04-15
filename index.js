a716d75e2d84d6ff6fdf9ea2108c402071cb63f0
Bryan Stafford <BStaff@gmail.com>
2024-04-15 11:16:34 -0400

    uibuild

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require("cors")
 const app = express()
+
+// Middlewares
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('body', (req, res) => { 
    return JSON.stringify(req.body)
