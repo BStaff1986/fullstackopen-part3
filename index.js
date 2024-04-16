a48fe2a32933e545e33e3fed1647b1ba93ccf565
Bryan Stafford <BStaff@gmail.com>
2024-04-16 13:12:24 -0400

    Finished exercise 3.18

--- index.js
+++ index.js
@@ -16,28 +16,38 @@ morgan.token('body', (req, res) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (request, response) => {
-    PhoneNumber.find({}).then(result => {
-        response.json(result)
-    })
+    PhoneNumber.find({})
+        .then(result => {
+            response.json(result)
+        })
+        .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    PhoneNumber.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+app.get('/api/persons/:id', (request, response, next) => {
+    PhoneNumber.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
     const currentDate = new Date().toLocaleString();
     const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
-    const numberOfPersons = persons.length;
-
-    response.send(
-        `
-        <p>Phonebook has info for ${numberOfPersons} people</p>
-        <p>${currentDate} (${timeZone})</p>
-        `
-    )
+    PhoneNumber.find({})
+        .then(result => {
+            const numberOfPersons = result.length.toString();
+            response.send(
+                `
+                <p>Phonebook has info for ${numberOfPersons} people</p>
+                <p>${currentDate} (${timeZone})</p>
+                `
+            )
+        })
 })
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -60,24 +70,50 @@ app.post('/api/persons', (request, response) => {
         name: body.name,
         number: body.number
     })
-    
-    number.save().then(savedNumber => {
-        response.json(savedNumber)        
-    })
 
+    number.save()
+        .then(savedNumber => {
+            response.json(savedNumber)
+        })
+        .catch(error => next(error))
 })
 
-app.delete("/api/persons/:id", (request, response) => {
+app.put("/api/persons/:id", (request, response, next) => {
+    const body = request.body;
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    PhoneNumber.findByIdAndUpdate(request.params.id, person, { "new": true })
+        .then(result => {
+            response.json(result)
+        })
+        .catch(error => next(error))
+})
+
+app.delete("/api/persons/:id", (request, response, next) => {
     PhoneNumber.findByIdAndDelete(request.params.id)
-    .then(result => {
-        response.status(204).end()
-    })
-    .catch(error => {
-        console.log(error);
-    })
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => {
+            next(error);
+        })
 })
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: "Malformatted id" })
+    }
+}
+
+app.use(errorHandler)
